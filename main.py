b400f702c898231641258fd3287834bd70de2594
Yanfeng Liu <yanfengliux@gmail.com>
2022-12-11 23:26:24 -0800

    set framerate

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 
 size = width, height = 640, 480
+framerate = 60
 speed = [2, 2]
 black = 0, 0, 0
 flags = pygame.SCALED
@@ -10,7 +11,7 @@ flags = pygame.SCALED
 screen = pygame.display.set_mode(size, flags, vsync=1)
 
 ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+ball_rect = ball.get_rect()
 
 clock = pygame.time.Clock()
 
@@ -19,12 +20,13 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    clock.tick(framerate)
+    ball_rect = ball_rect.move(speed)
+    if ball_rect.left < 0 or ball_rect.right > width:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ball_rect.top < 0 or ball_rect.bottom > height:
         speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(ball, ball_rect)
     pygame.display.flip()
