e0f9f1f860a43ddc500ab98879e4d74d38ac71b8
Yanfeng Liu <yanfengliux@gmail.com>
2022-12-11 23:07:16 -0800

    bouncing ball example

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import sys
 import pygame
+pygame.init()
 
+size = width, height = 640, 480
+speed = [2, 2]
+black = 0, 0, 0
+flags = pygame.SCALED
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((240, 180))
-    running = True
+screen = pygame.display.set_mode(size, flags, vsync=1)
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
 
+clock = pygame.time.Clock()
 
-if __name__ == "__main__":
-    main()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
