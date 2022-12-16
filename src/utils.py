f623dea902f5e31954f16f5d246c58d9dee97973
Yanfeng Liu <yanfengliux@gmail.com>
2022-12-15 21:27:19 -0800

    pad random position range

--- src/utils.py
+++ src/utils.py
@@ -16,13 +16,20 @@ from type import Color, Point
 
 
 def get_random_position(width: int, height: int) -> Point:
+    padding_ratio = 0.1
     return {
-        "left": int(np.random.rand() * width),
-        "top": int(np.random.rand() * height),
+        "left": int(
+            width * (padding_ratio + np.random.rand() * (1 - padding_ratio * 2))
+        ),
+        "top": int(
+            height * (padding_ratio + np.random.rand() * (1 - padding_ratio * 2))
+        ),
     }
+
 
 def get_random_color() -> Color:
     return tuple(255 * np.asarray(colorsys.hsv_to_rgb(np.random.rand(), 1.0, 1.0)))
+
 
 def get_random_shape(shape_type_list: List[ShapeType], size: int) -> Shape:
     color = get_random_color()
@@ -32,24 +39,30 @@ def get_random_shape(shape_type_list: List[ShapeType], size: int) -> Shape:
     elif shape_type == ShapeType.CIRCLE:
         return Circle(color=color, radius=size)
 
+
 station_shape_list = [ShapeType.RECT, ShapeType.CIRCLE]
+
 
 def get_random_station_shape() -> Shape:
     return get_random_shape(station_shape_list, station_size)
 
+
 def get_random_passenger_shape() -> Shape:
     return get_random_shape(station_shape_list, passenger_size)
+
 
 def get_random_station() -> Station:
     shape = get_random_station_shape()
     position = get_random_position(screen_width, screen_height)
     return Station(shape, position)
 
+
 def get_random_stations(num: int) -> List[Station]:
     stations: List[Station] = []
     for _ in range(num):
         stations.append(get_random_station())
     return stations
+
 
 def get_metros(num: int) -> List[Metro]:
     metros: List[Metro] = []
