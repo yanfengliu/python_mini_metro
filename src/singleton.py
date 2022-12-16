460bc34ad021c509007a785da0b1a2f89d9902f9
Yanfeng Liu <yanfengliux@gmail.com>
2022-12-15 23:30:59 -0800

    remove `print` statement

--- src/singleton.py
+++ src/singleton.py
@@ -3,6 +3,5 @@ class Singleton:
 
     def __new__(cls):
         if cls._instance is None:
-            print("Creating the object")
             cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
