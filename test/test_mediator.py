97e68a33d7667670e423f0836ee01b436a67c483
Yanfeng Liu <yanfengliux@gmail.com>
2022-12-17 13:15:28 -0800

    add tests for loop

--- test/test_mediator.py
+++ test/test_mediator.py
@@ -95,6 +95,50 @@ class TestMediator(unittest.TestCase):
         self.mediator.react(MouseEvent(EventType.MOUSE_UP, Point(0, 1)))
         self.assertEqual(len(self.mediator.paths), 0)
 
+    def test_mouse_dragged_between_3_stations_creates_looped_path(self):
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_DOWN, self.mediator.stations[0].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_MOTION, self.mediator.stations[1].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_MOTION, self.mediator.stations[2].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_UP, self.mediator.stations[0].position)
+        )
+        self.assertEqual(len(self.mediator.paths), 1)
+        self.assertTrue(self.mediator.paths[0].is_looped)
+
+    def test_path_between_2_stations_is_not_looped(self):
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_DOWN, self.mediator.stations[0].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_MOTION, self.mediator.stations[1].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_UP, self.mediator.stations[1].position)
+        )
+        self.assertEqual(len(self.mediator.paths), 1)
+        self.assertFalse(self.mediator.paths[0].is_looped)
+
+    def test_mouse_dragged_between_3_stations_without_coming_back_to_first_does_not_create_loop(
+        self,
+    ):
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_DOWN, self.mediator.stations[0].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_MOTION, self.mediator.stations[1].position)
+        )
+        self.mediator.react(
+            MouseEvent(EventType.MOUSE_UP, self.mediator.stations[2].position)
+        )
+        self.assertEqual(len(self.mediator.paths), 1)
+        self.assertFalse(self.mediator.paths[0].is_looped)
+
 
 if __name__ == "__main__":
     unittest.main()
--- test/test_path.py
+++ test/test_path.py
@@ -1,16 +1,20 @@
 import os
 import sys
 import unittest
+from math import ceil
 from unittest.mock import MagicMock, create_autospec, patch
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
 import pygame
 
+from config import framerate, metro_speed_per_ms
 from entity.get_entity import get_random_station, get_random_stations
+from entity.metro import Metro
 from entity.path import Path
+from entity.station import Station
 from geometry.point import Point
-from utils import get_random_color, get_random_position
+from utils import get_random_color, get_random_position, get_random_station_shape
 
 
 class TestPath(unittest.TestCase):
@@ -42,6 +46,68 @@ class TestPath(unittest.TestCase):
         path.set_temporary_point(Point(1, 1))
         path.draw(self.screen)
         self.assertEqual(pygame.draw.line.call_count, 1)
+
+    def test_metro_starts_at_beginning_of_first_line(self):
+        path = Path()
+        path.add_station(get_random_station())
+        path.add_station(get_random_station())
+        path.draw(self.screen)
+        metro = Metro()
+        path.add_metro(metro)
+        self.assertEqual(metro.current_line, path.lines[0])
+        self.assertEqual(metro.current_line_idx, 0)
+        self.assertTrue(metro.is_forward)
+
+    def test_metro_moves_from_beginning_to_end(self):
+        path = Path()
+        path.add_station(Station(get_random_station_shape(), Point(0, 0)))
+        dist_in_one_sec = 1000 * metro_speed_per_ms
+        path.add_station(Station(get_random_station_shape(), Point(dist_in_one_sec, 0)))
+        path.draw(self.screen)
+        for station in path.stations:
+            station.draw(self.screen)
+        metro = Metro()
+        path.add_metro(metro)
+        dt_ms = ceil(1000 / framerate)
+        for _ in range(framerate):
+            path.move_metro(metro, dt_ms)
+        self.assertTrue(path.stations[1].contains(metro.position))
+
+    def test_metro_turns_around_when_it_reaches_the_end(self):
+        path = Path()
+        path.add_station(Station(get_random_station_shape(), Point(0, 0)))
+        dist_in_one_sec = 1000 * metro_speed_per_ms
+        path.add_station(Station(get_random_station_shape(), Point(dist_in_one_sec, 0)))
+        path.draw(self.screen)
+        for station in path.stations:
+            station.draw(self.screen)
+        metro = Metro()
+        path.add_metro(metro)
+        dt_ms = ceil(1000 / framerate)
+        for _ in range(framerate + 1):
+            path.move_metro(metro, dt_ms)
+        self.assertFalse(metro.is_forward)
+
+    def test_metro_loops_around_the_path(self):
+        path = Path()
+        path.add_station(Station(get_random_station_shape(), Point(0, 0)))
+        dist_in_one_sec = 1000 * metro_speed_per_ms
+        path.add_station(Station(get_random_station_shape(), Point(dist_in_one_sec, 0)))
+        path.add_station(
+            Station(get_random_station_shape(), Point(dist_in_one_sec, dist_in_one_sec))
+        )
+        path.add_station(Station(get_random_station_shape(), Point(0, dist_in_one_sec)))
+        path.set_loop()
+        path.draw(self.screen)
+        for station in path.stations:
+            station.draw(self.screen)
+        metro = Metro()
+        path.add_metro(metro)
+        dt_ms = ceil(1000 / framerate)
+        for station_idx in [1, 2, 3, 0, 1]:
+            for _ in range(framerate):
+                path.move_metro(metro, dt_ms)
+            self.assertTrue(path.stations[station_idx].contains(metro.position))
 
 
 if __name__ == "__main__":
