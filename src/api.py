df278ffe2d94551861ff18f42ff31cd04e514af5
waltsai2483 <waltsai930830@gmail.com>
2025-05-16 14:59:12 +0800

    Docstring

--- src/api.py
+++ src/api.py
@@ -4,8 +4,16 @@ import pygame
 from config import framerate, gamespeed, num_stations_max, screen_color, screen_height, screen_width, num_stations_max
 from mediator import Mediator
 
-class Game:
+class StationDeterminedGame:
+    """
+       A game simulator. When instantiating the simulator all stations are generated randomly.
+       Call StationDeterminedGame::run() to start the game with same stations and given paths.
+    """
     def __init__(self, visuals: bool = False):
+        """
+            Initialize the game simulator.
+            If visuals is true, it will display full game simulation on the screen with some speed costs.
+        """
         self.mediator = Mediator(gen_stations_first=True)
         self.clock = pygame.time.Clock()
         self.visuals = visuals
@@ -17,6 +25,11 @@ class Game:
             self.mediator.assign_paths_to_buttons()
 
     def run(self, *paths) -> int:
+        """
+            Runs the game with the given initial paths.
+            Each path has the format ([station1, station2, ...], is_looped).
+            Returns the final score.
+        """
         self.mediator.initialize_paths(*paths)
         game_over = False
         while not game_over:
@@ -27,5 +40,3 @@ class Game:
                 self.mediator.render(self.screen)
                 pygame.display.flip()
         return self.mediator.score
-
-print(Game(visuals=True).run((range(num_stations_max), False)))
