8f9ea63de12f379dbb507793ee0d4e25192ec100
waltsai2483 <waltsai930830@gmail.com>
2025-05-16 15:20:14 +0800

    Small change to doc & adjust game speed

--- src/api.py
+++ src/api.py
@@ -1,4 +1,4 @@
-from ast import List, Tuple
+from typing import List, Tuple
 import pygame
 
 from config import framerate, gamespeed, num_stations_max, screen_color, screen_height, screen_width, num_stations_max
@@ -6,15 +6,16 @@ from mediator import Mediator
 
 class StationDeterminedGame:
     """
-       A game simulator. When instantiating the simulator all stations are generated randomly.
+       A game simulator. When instantiating the simulator all stations are generated randomly.<br/>
        Call StationDeterminedGame::run() to start the game with same stations and given paths.
     """
     def __init__(self, visuals: bool = False):
         """
-            Initialize the game simulator.
+            Initialize the game simulator.<br/>
             If visuals is true, it will display full game simulation on the screen with some speed costs.
         """
         self.mediator = Mediator(gen_stations_first=True)
+        self.gamespeed = 250
         self.clock = pygame.time.Clock()
         self.visuals = visuals
 
@@ -23,17 +24,23 @@ class StationDeterminedGame:
             flags = pygame.SCALED
             self.screen = pygame.display.set_mode((screen_width, screen_height), flags, vsync=1)
             self.mediator.assign_paths_to_buttons()
+    
+    def set_game_speed(self, gamespeed: float):
+        self.gamespeed = gamespeed
 
-    def run(self, *paths) -> int:
+    def run(self, *paths: Tuple[List[int], bool]) -> int:
         """
-            Runs the game with the given initial paths.
-            Each path has the format ([station1, station2, ...], is_looped).
-            Returns the final score.
+            Runs the game with the given initial paths.<br/>
+            Each path has the format ([0, 1, ...], is_looped) as path: station1 -> station2 -> ... -> back to start if looped.<br/>
+            Returns the final score.<br/><br/>
+            Example:<br/>
+            game = StationDeterminedGame()<br/>
+            game.run(([0, 1, 2, 3, 4], True))
         """
         self.mediator.initialize_paths(*paths)
         game_over = False
         while not game_over:
-            dt_ms = self.clock.tick(framerate) * gamespeed
+            dt_ms = self.clock.tick(framerate) * self.gamespeed
             game_over = self.mediator.increment_time(dt_ms)
             if self.visuals:
                 self.screen.fill(screen_color)
