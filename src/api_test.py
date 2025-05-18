8b1a33529340be45b11f76bffb74f7eb1ca4de37
Phantom0174 <darrenhsiou@gmail.com>
2025-05-18 19:37:34 +0800

    add timer

--- src/api_test.py
+++ src/api_test.py
@@ -1,12 +1,28 @@
 from api import StaticStationGame
 
+from timer import Timer
 
-game = StaticStationGame(gamespeed=100, visuals=True)
+timer = Timer()
+game = StaticStationGame(gamespeed=50, visuals=True)
+
+timer.start()
 
 print(game.run(
-    ([0, 2, 4, 6, 8], True)
+    ([0, 2, 4, 6, 8], True),
+    ([0, 1, 3, 5, 7], True),
+    ([1, 3, 7, 9, 5], True)
 ))
+
+print("v=True, Time taken:", timer.end())
+
 game.visuals = False
+
+timer.start()
+
 print(game.run(
-    ([0, 2, 4, 6, 8], True)
+    ([0, 2, 4, 6, 8], True),
+    ([0, 1, 3, 5, 7], True),
+    ([1, 3, 7, 9, 5], True)
 ))
+
+print("v=False, Time taken:", timer.end())
